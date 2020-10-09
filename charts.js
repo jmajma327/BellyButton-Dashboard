ed16382afaeb35085370ecd68cf7991d4269c675
jmajma327 <prinmaxx1@msn.com>
2020-10-09 10:24:44 -0400

    update

--- charts.js
+++ charts.js
@@ -156,7 +156,7 @@ var washResult = washResultArray[0];
     ];
     
     // 5. Create the layout for the gauge chart.
-    var gaugeLayout = { width: 400, height: 250, margin: {t: 0, b: 0} 
+    var gaugeLayout = { width: 500, height: 400, margin: {t: 0, b: 0} 
     };
 
     // 6. Use Plotly to plot the gauge data and layout.
